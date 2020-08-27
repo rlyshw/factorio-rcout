a6ae076e4c3bb62a0b423023d40e78f123632aa2
Ubuntu <rlyshw@factorio.rlyshw.home>
2020-08-27 16:13:44 +0000

    removed prefixes on output

--- control.lua
+++ control.lua
@@ -3,27 +3,27 @@ commands.add_command("itemoutstats", "send stats", function(table)
   local player = game.get_player(1)
   local output_counts = player.force.item_production_statistics.output_counts
   for k,v in pairs(output_counts) do
-    rcon.print("itemout-"..k..":"..v)
+    rcon.print(k..":"..v)
   end
 end)
 commands.add_command("iteminstats", "send stats", function(table)
   local player = game.get_player(1)
   local output_counts = player.force.item_production_statistics.input_counts
   for k,v in pairs(output_counts) do
-    rcon.print("itemin-"..k..":"..v)
+    rcon.print(k..":"..v)
   end
 end)
 commands.add_command("fluidoutstats", "send stats", function(table)
   local player = game.get_player(1)
   local output_counts = player.force.fluid_production_statistics.output_counts
   for k,v in pairs(output_counts) do
-    rcon.print("fluidout-"..k..":"..v)
+    rcon.print(k..":"..v)
   end
 end)
 commands.add_command("fluidinstats", "send stats", function(table)
   local player = game.get_player(1)
   local output_counts = player.force.fluid_production_statistics.input_counts
   for k,v in pairs(output_counts) do
-    rcon.print("fluidin-"..k..":"..v)
+    rcon.print(k..":"..v)
   end
 end)
